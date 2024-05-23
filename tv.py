cd628a0545f85356553ce65f8f53de72f732bcb9
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-05-24 06:55:54 +0800

    Added Pseudocode

---
+++ tv.py
@@ -0,0 +1,11 @@
+# Create a class
+# Create constructor
+# Instance Variable
+# Create the turn on method
+# Create the turn off method
+# Set the channel
+# Get the volume
+# Channel up
+# Channel down
+# Volume up
+# Volume down
