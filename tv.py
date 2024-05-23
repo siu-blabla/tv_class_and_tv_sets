873c36df68533a465b66a3baca19bb2d8986f58d
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-05-24 07:06:28 +0800

    Added instance variable for TV on

--- tv.py
+++ tv.py
@@ -1,10 +1,11 @@
 # Create a class
 class TV:
     # Create constructor
-    def __init__(self) :
+    def __init__(self):
         # Instance Variable
         self.channel = 1
         self.volume_level = 1
+        self.is_on = False
 
     # Create the turn on method
     # Create the turn off method
