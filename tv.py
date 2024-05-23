275e400117d99008179eadd02f87595c6cf0db30
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-05-24 07:01:34 +0800

    Created constructor

--- tv.py
+++ tv.py
@@ -1,12 +1,13 @@
 # Create a class
 class TV:
-# Create constructor
-# Instance Variable
-# Create the turn on method
-# Create the turn off method
-# Set the channel
-# Get the volume
-# Channel up
-# Channel down
-# Volume up
-# Volume down
+    # Create constructor
+    def __init__(self) :
+        # Instance Variable
+    # Create the turn on method
+    # Create the turn off method
+    # Set the channel
+    # Get the volume
+    # Channel up
+    # Channel down
+    # Volume up
+    # Volume down
