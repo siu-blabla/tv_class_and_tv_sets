a77389d316b79a9ca6892f5f19ed69f5a8841483
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-05-24 06:57:17 +0800

    Added pseudocode for test_tv.py

---
+++ test_tv.py
@@ -0,0 +1,7 @@
+# Import the tv class
+# Create new object
+# Set the channel of tv1 to 30
+# Set the volume of tv1 to 3
+# Set the channel of tv2 to 3
+# Set the volume of tv2 to 2
+# Print the output channel and volume level of tv1 and tv2
