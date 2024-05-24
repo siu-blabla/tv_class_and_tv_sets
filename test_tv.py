7e8319060e51368a1bc2b06bef09661dcd2a8c7f
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-05-24 08:06:19 +0800

    Importing the tv class and created new object

--- test_tv.py
+++ test_tv.py
@@ -1,5 +1,10 @@
 # Import the tv class
+from tv import TV
+
 # Create new object
+tv1 = TV()
+tv2 = TV()
+
 # Set the channel of tv1 to 30
 # Set the volume of tv1 to 3
 # Set the channel of tv2 to 3
