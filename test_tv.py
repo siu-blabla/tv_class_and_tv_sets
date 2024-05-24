49d7ed76cda0fd8bb636ff2214d44567ecb2afe3
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-05-24 08:35:45 +0800

    Added some lines and removed the pyfiglet module

--- test_tv.py
+++ test_tv.py
@@ -1,6 +1,5 @@
 # Import the tv class
 from tv import TV
-from pyfiglet import Figlet
 
 # Create new object
 tv1 = TV()
@@ -19,5 +18,8 @@ tv2.set_channel(3)
 tv2.set_volume(2)
 
 # Print the output channel and volume level of tv1 and tv2
+print("------------------------------------------")
 print(f"\033[93m\033[1mtv1's channel is {tv1.get_channel()} and volume level is {tv1.get_volume()}")
 print(f"tv2's channel is {tv2.get_channel()} and volume level is {tv2.get_volume()}\033[0m")
+print("------------------------------------------")
+
