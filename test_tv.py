74a452837c9b28a442bc817db2a488338d272701
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-05-24 08:09:07 +0800

    Setting the channel for tv2

--- test_tv.py
+++ test_tv.py
@@ -12,5 +12,7 @@ tv1.set_channel(30)
 tv1.set_volume(3)
 
 # Set the channel of tv2 to 3
+tv2.set_channel(3)
+
 # Set the volume of tv2 to 2
 # Print the output channel and volume level of tv1 and tv2
